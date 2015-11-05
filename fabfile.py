22e4ca5c2090cf3b632e41335560d3f53bc87a27
CorcovadoMing <rf37535@gmail.com>
2015-11-06 00:37:25 +0800

    Done initial the topology

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,8 @@ from fabric.context_managers import hide
 
 hosts = []
 switches = []
+ips = 2
+faces = {}
 links = {}
 
 def minishell():
@@ -34,6 +36,40 @@ def addSwitch(switches, mode="secure"):
         local("sudo ovs-vsctl add-br " + s)
         local("sudo ovs-vsctl set-fail-mode " + s + " " + mode)
 
+def addLink(links):
+    global ips
+    fr_type = None
+    to_type = None
+    s = None
+    h = None
+    
+    for key in links:
+        fr, to = key, links[key]
+        if fr in hosts:
+            fr_type = "host"
+            h = fr
+        elif fr in switches:
+            fr_type = "switch"
+            s = fr
+        if to in hosts:
+            to_type = "host"
+            h = to
+        elif to in switches:
+            to_type = "switch"
+            s = to
+
+        if fr_type != to_type: # link switch and host
+            local("sudo pipework " + s + " -l " + s + "-eth" + str(faces[s]) + " " + h + " 192.168.1." + str(ips) + "/24")
+            faces[s] += 1
+            ips += 1
+        else: # link switch to switch
+            local("sudo ip link add name " + fr + "-eth" + str(faces[fr]) + " type veth peer name " + to + "-eth" + str(faces[to]))
+            local("sudo ovs-vsctl add-port " + fr + " " + fr + "-eth" + str(faces[fr]))
+            local("sudo ovs-vsctl add-port " + to + " " + to + "-eth" + str(faces[to]))
+            faces[fr] += 1
+            faces[to] += 1
+
+
 def addHost(hosts):
     for h in hosts:
         local("docker run -d --name " + h + " rf37535/nfd nfd", capture=True)
@@ -48,6 +84,7 @@ def parseTopo(topo):
                 hosts.append(line.split()[0])
             elif parseflag[1] and len(line.split()):
                 switches.append(line.split()[0])
+                faces[line.split()[0]] = 1
             elif parseflag[2] and len(line.split()):
                 fr, to = line.split()
                 links[fr] = to
@@ -61,33 +98,28 @@ def parseTopo(topo):
             elif not len(line.split()):
                 parseflag = [False, False, False]
 
-           
-       
-
 def up(topo='', script=''):
+    global hosts, switches, links, faces
     with hide('running'):
         if topo != '':
             parseTopo(topo)
-            print hosts
-            print switches
-            print links
-            minishell()
-        else: # Default topology (h1 -- s1 -- s2 -- h2)       
-            addSwitch(["s1", "s2"])
-            local("sudo ip link add name s1-eth1 type veth peer name s2-eth1")
-            local("sudo ovs-vsctl add-port s1 s1-eth1")
-            local("sudo ovs-vsctl add-port s2 s2-eth1")
-            addHost(["h1", "h2"])
-            local("sudo pipework s1 -l s1-eth0 h1 192.168.1.2/24")
-            local("sudo pipework s2 -l s2-eth0 h2 192.168.1.3/24")
-            minishell()
+        else: # Default topology (h1 -- s1 -- s2 -- h2)
+            hosts = ['h1', 'h2']
+            switches = ['s1', 's2']
+            faces = {'s1':1, 's2':1}
+            links = {'h1':'s1', 'h2':'s2', 's1':'s2'}
+        addSwitch(switches)
+        addHost(hosts)
+        addLink(links)
+        minishell()
+
 
 
 def down():
     with hide('running'):
         print yellow("*** Shut down NDN hosts", bold=True)
-        local("docker rm -f h1", capture=True)
-        local("docker rm -f h2", capture=True)
+        for _ in hosts:
+            local("docker rm -f " + _, capture=True)
         print yellow("*** Clean the Open vSwitch", bold=True)
         local("sudo mn -c", capture=True)
         print yellow("*** bye", bold=True)
